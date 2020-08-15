b3583644171ca7ed4cfe67feb94c1093e85b5c7b
Lucas Alcantara <lucaspe0685@gmail.com>
2020-08-14 23:27:45 -0300

    Implementou função excluir

--- App de Todos JS/Todos.js
+++ App de Todos JS/Todos.js
@@ -14,8 +14,19 @@ function renderTodos() {
     for (todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
+    
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos +')')
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
 
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
     }
 }
@@ -27,7 +38,13 @@ function addTodo(){
 
     todos.push(todoText);
     inputElement.value = '';renderTodos();
-
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+
+}
+
