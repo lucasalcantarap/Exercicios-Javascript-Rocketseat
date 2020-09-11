717bc469155ee3ac866d14818ee8ff45d645b2d1
lucasalcantarap <lucaspe0685@gmail.com>
2020-09-10 21:34:07 -0300

    add exercicios de JS

--- App de Todos JS/Todos.js
+++ App de Todos JS/Todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) ||[];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -38,6 +34,7 @@ function addTodo(){
 
     todos.push(todoText);
     inputElement.value = '';renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -45,6 +42,10 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos();
-
+    saveToStorage();
 }
 
+function saveToStorage() {
+    
+localStorage.setItem('list_todos', JSON.stringify(todos));
+}
