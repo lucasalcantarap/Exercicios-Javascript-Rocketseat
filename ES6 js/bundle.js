eb3d6999949ca3f1444fec7b6b4f0d96b70080c7
lucasalcantarap <lucaspe0685@gmail.com>
2020-11-28 20:52:56 -0300

    corrigido caminho do script

--- ES6 js/bundle.js
+++ ES6 js/bundle.js
@@ -2,6 +2,32 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var TodoList = function TodoList() {
-  _classCallCheck(this, TodoList);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var TodoList = /*#__PURE__*/function () {
+  function TodoList() {
+    _classCallCheck(this, TodoList);
+
+    //metodo constructor
+    this.todos = [];
+  }
+
+  _createClass(TodoList, [{
+    key: "addTodo",
+    value: function addTodo() {
+      //metodo para adicionar Todo
+      this.todos.push('Novo todo');
+      console.log(this.todos);
+    }
+  }]);
+
+  return TodoList;
+}();
+
+var MinhaLista = new TodoList(); //instaciação de método
+
+document.getElementById('novotodo').onclick = function () {
+  MinhaLista.addTodo();
 };
