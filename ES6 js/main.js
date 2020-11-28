5c8a8ab1a627acd6a245d43ba0b98fdc9d98c671
lucasalcantarap <lucaspe0685@gmail.com>
2020-11-28 20:44:04 -0300

    instaciação de metodo

--- ES6 js/main.js
+++ ES6 js/main.js
@@ -1,5 +1,17 @@
 class TodoList {
-    constructor(){
-        
+    constructor(){ //metodo constructor
+        this.todos = []; 
     }
+
+
+addTodo() { //metodo para adicionar Todo
+    this.todos.push('Novo todo');
+    console.log(this.todos);
+    }
+}
+
+const MinhaLista = new TodoList(); //instaciação de método
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.addTodo();
 }
